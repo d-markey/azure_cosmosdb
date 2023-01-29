fc33fcabf74d9e2882ae44f26a031de8279c763e
d-markey <d-markey@users.noreply.github.com>
2023-01-30 00:45:33 +0100

    runtests.bat: check test status before going on with coverage report

--- tool/runtests.bat
+++ tool/runtests.bat
@@ -29,12 +29,17 @@ SHIFT
 GOTO parse
 :endparse
 
-rmdir .\test\coverage /s /q
-mkdir .\test\coverage
+RMDIR .\test\coverage /S /Q
+MKDIR .\test\coverage
 CALL dart test --coverage=.\test\coverage %__ARGS__%
+SET __ERRLVL__=%ERRORLEVEL%
+IF %__ERRLVL__% NEQ 0 (
+    ECHO Tests failed with exit code = %__ERRLVL__%
+    EXIT /B %__ERRLVL__%
+)
 IF "%__COV__%" == "1" (
     CALL dart run coverage:format_coverage --packages=.dart_tool\package_config.json --report-on=lib --lcov -o .\test\coverage\lcov.info -i .\test\coverage
-    rmdir .\coverage /s /q
+    RMDIR .\coverage /s /q
     java -jar .\tool\jgenhtml\jgenhtml-1.5.jar .\test\coverage\lcov.info --output-directory .\coverage  
     CALL dart run .\tool\xtractcov\main.dart
 
